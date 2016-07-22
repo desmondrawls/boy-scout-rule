acb8d170ddc34264fc42ef77e02c506f845f4ffa
Desmond Pompa Alarcon Rawls and Scott Hajek <pair+drawls+shajek@pivotal.io>
2016-07-22 10:40:27 -0700

    Add rspec.
    - no real specs yet.

---
+++ spec/merge_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe BoyScoutRule do
+  it "tells the user that their merge is good" do
+    true.should == true
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,5 @@
+require 'boy-scout-rule'
+
+RSpec.configure do |config|
+  # some (optional) config here
+end
